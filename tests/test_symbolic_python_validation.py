540bf0d269e7ce5d66316741cad96f2bf2bab241
connect.vin <38634222+2vin@users.noreply.github.com>
2026-09-17 23:02:58 +0530

    Add files via upload

---
+++ tests/test_symbolic_python_validation.py
@@ -0,0 +1,39 @@
+from teacher_agent.validator import validate_python_blocks
+
+
+def test_symbolic_image_indexing_is_syntax_only():
+    assert validate_python_blocks(
+        '```python\\nimage[row][column][channel]\\n```'
+    ) == []
+
+
+def test_undefined_assignment_is_still_rejected():
+    errors = validate_python_blocks(
+        '```python\\npixel = image[row][column][channel]\\nprint(pixel)\\n```'
+    )
+    assert errors
+    assert 'NameError' in errors[0]
+
+
+def test_function_call_is_still_rejected():
+    errors = validate_python_blocks(
+        '```python\\nshow_pixel(image[row][column][channel])\\n```'
+    )
+    assert errors
+    assert 'NameError' in errors[0]
+
+
+def test_runtime_math_error_is_still_rejected():
+    errors = validate_python_blocks(
+        '```python\\nvalue = 1 / 0\\n```'
+    )
+    assert errors
+    assert 'ZeroDivisionError' in errors[0]
+
+
+def test_symbolic_expression_still_must_be_valid_python():
+    errors = validate_python_blocks(
+        '```python\\nimage[row][column][channel\\n```'
+    )
+    assert errors
+    assert 'syntax error' in errors[0].lower()
